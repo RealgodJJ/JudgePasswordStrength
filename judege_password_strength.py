d1e073c3e7c38ea95fc0750606aa82a9f90873fc
maaonan <xiaozhiqiu13@sina.com>
2018-10-03 12:27:45 +0800

    Use class PasswordTool.

--- judege_password_strength.py
+++ judege_password_strength.py
@@ -7,22 +7,46 @@
 """
 
 
-def check_number_exist(password):
-    has_number = False
-    for value in password:
-        if value.isnumeric():
-            has_number = True
-            break
-    return has_number
+class PasswordTool:
+    """
+        密码工具类
+    """
 
+    def __init__(self, password):
+        self.password = password
+        self.strength = 0
 
-def check_alphabet_exist(password):
-    has_alphabet = False
-    for value in password:
-        if value.isalpha():
-            has_alphabet = True
-            break
-    return has_alphabet
+    def process_password(self):
+        if len(self.password) >= 8:
+            self.strength += 1
+        else:
+            print("密码的长度未超过8位！")
+
+        if self.check_number_exist():
+            self.strength += 1
+        else:
+            print("密码不包含数字！")
+
+        if self.check_alphabet_exist():
+            self.strength += 1
+        else:
+            print("密码不包含字母！")
+
+    def check_number_exist(self):
+        has_number = False
+        for value in self.password:
+            if value.isnumeric():
+                has_number = True
+                break
+        return has_number
+
+    def check_alphabet_exist(self):
+        has_alphabet = False
+        for value in self.password:
+            if value.isalpha():
+                has_alphabet = True
+                break
+        return has_alphabet
 
 
 def main():
@@ -30,25 +54,12 @@ def main():
 
     while try_times > 0:
         password = input("请输入设置的密码：\n")
-        strength = 0
-
-        if len(password) >= 8:
-            strength += 1
-        else:
-            print("密码的长度未超过8位！")
-
-        if check_number_exist(password):
-            strength += 1
-        else:
-            print("密码不包含数字！")
-
-        if check_alphabet_exist(password):
-            strength += 1
-        else:
-            print("密码不包含字母！")
+        # 实例化对象
+        password_tool = PasswordTool(password)
+        password_tool.process_password()
 
         file = open("password.txt", "a")
-        file.write("密码：{}   强度：{}\n".format(password, strength))
+        file.write("密码：{}   强度：{}\n".format(password, password_tool.strength))
         file.close()
 
         # style 1
@@ -58,25 +69,14 @@ def main():
         print(content)
         file.close()
 
-        # style 2
-        # print("====== Style 2 ======")
-        # file = open("password.txt", "r")
-        # line = file.readline()
-        # print(line)
-        # line = file.readline()
-        # print(line)
-        # line = file.readline()
-        # print(line)
-        # file.close()
-
         # style 3
-        print("====== Style 3 ======")
+        print("====== Style 2 ======")
         file = open("password.txt", "r")
         for item, line in enumerate(file.readlines()):
             print("{}.{}".format(item + 1, line))
         file.close()
 
-        if strength == 3:
+        if password_tool.strength == 3:
             print("密码格式设置正确！\n")
             break
         else:
